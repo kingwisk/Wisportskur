47bde5a4336a83a06840843aa7660e5c8484a4b1
kingwisk <kingwisk@vt.edu>
2017-06-30 00:20:09 -0400

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://kingwisk.Wisportskur.io'
+SITEURL = 'http://kingwisk.Wisportskur'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
